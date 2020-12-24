6ef26da318ec735e6956f8de0961e897bb16c2d3
Rafael Reyes <rafaelalexanderr5@gmail.com>
2020-12-24 18:41:37 -0400

    ADD - recepi list model and layout

--- project-course/src/app/Components/recipes/recipe-list/recipe-list.component.ts
+++ project-course/src/app/Components/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('Test Name', 'Test Description', 'https://www.telegraph.co.uk/content/dam/food-and-drink/2019/01/11/TELEMMGLPICT000185036503_trans_NvBQzQNjv4Bq8m3xuhMyFOjUOkuEnTdW-M-bhHwB87o-r13mliye62g.jpeg?imwidth=1400')
+    
+  ];
   constructor() { }
 
   ngOnInit(): void {
