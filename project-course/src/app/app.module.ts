9b66c203e63074bd4c190bff8d6111fbfa47b280
Rafael Reyes <rafaelalexanderr5@gmail.com>
2020-12-24 14:12:33 -0400

    UPDATE - Components arquitecture

--- project-course/src/app/app.module.ts
+++ project-course/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header/header.component';
 import { ShoppingListComponent } from './Feature/shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './Components/shopping-list-edit/shopping-list-edit.component';
+import { ShoppingListEditComponent } from './Components/shopping/shopping-list-edit/shopping-list-edit.component';
 import { IngredientComponent } from './Model/ingredient/ingredient.component';
 import { RecipeBookComponent } from './Feature/recipe-book/recipe-book.component';
-import { RecipeListComponent } from './Components/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './Components/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './Components/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './Components/recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './Components/recipes/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './Components/recipes/recipe-detail/recipe-detail.component';
 import { RecipeComponent } from './Model/recipe/recipe.component';
+import { RecipesComponent } from './Components/recipes/recipes.component';
+import { ShoppingComponent } from './components/shopping/shopping.component';
 
 
 @NgModule({
@@ -25,6 +27,8 @@ import { RecipeComponent } from './Model/recipe/recipe.component';
     RecipeItemComponent,
     RecipeDetailComponent,
     RecipeComponent,
+    RecipesComponent,
+    ShoppingComponent,
   ],
   imports: [
     BrowserModule
