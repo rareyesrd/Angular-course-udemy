76ca0a35ff3408353aeec9c82bbabca164eff0f8
Rafael Reyes <r.reyes@caribemedia.com.do>
2020-12-29 17:00:39 -0400

    Shopping list - Add bottom works

--- project-course/src/app/Components/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ project-course/src/app/Components/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list-edit',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list-edit.component.css']
 })
 export class ShoppingListEditComponent implements OnInit {
-
+  @ViewChild('inputName', {static: true}) inputNameRef: ElementRef;
+  @ViewChild('inputAmount', {static: true}) inputAmountRef: ElementRef;
+  @Output() ingredient = new EventEmitter<Ingredient>();
   constructor() { }
-
   ngOnInit(): void {
   }
 
+  onAdd(): void{
+    const ingName = this.inputNameRef.nativeElement.value;
+    const ingAmount = this.inputAmountRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    this.ingredient.emit(newIngredient);
+  }
 }
