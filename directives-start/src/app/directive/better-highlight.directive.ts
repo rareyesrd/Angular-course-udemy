54bbdc414a56e9bfb202331625569eb7af9c6230
Rafael Reyes <rafaelalexanderr5@gmail.com>
2021-01-03 12:35:28 -0400

    Using directive with Input decorators

--- directives-start/src/app/directive/better-highlight.directive.ts
+++ directives-start/src/app/directive/better-highlight.directive.ts
@@ -3,6 +3,7 @@ import {
   ElementRef,
   HostBinding,
   HostListener,
+  Input,
   OnInit,
   Renderer2,
 } from "@angular/core";
@@ -11,21 +12,25 @@ import {
   selector: "[appBetterHighlight]",
 })
 export class BetterHighlightDirective implements OnInit {
+  @Input() defaultBackground: string = 'transparent';
+  @Input('appBetterHighlight') highlightBackground: string = 'blue';
+
   constructor(private el: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit(): void {
     // this.renderer.setStyle(this.el.nativeElement, 'background-color', 'blue');
     // this.renderer.setStyle(this.el.nativeElement, 'color', '#fff');
+    this.backgroundColor = this.defaultBackground;
     this.renderer.setStyle(this.el.nativeElement, "padding", "20px");
   }
-  @HostBinding("style.backgroundColor") backgroundColor: string = "transparent";
+  @HostBinding("style.backgroundColor") backgroundColor: string = this.defaultBackground;
   @HostBinding("style.color") color: string = "#000";
   @HostBinding("style.transition") transition: string = "all 1s ease";
 
   @HostListener("mouseenter") mouseover() {
     // this.renderer.setStyle(this.el.nativeElement, 'background-color', 'blue');
     // this.renderer.setStyle(this.el.nativeElement, 'color', '#fff');
-    this.backgroundColor = "blue";
+    this.backgroundColor = this.highlightBackground;
     this.color = "#fff";
     this.transition = "all .5s ease";
   }
@@ -33,7 +38,7 @@ export class BetterHighlightDirective implements OnInit {
   @HostListener("mouseleave") mouseleave() {
     // this.renderer.setStyle(this.el.nativeElement, 'background-color', 'transparent');
     // this.renderer.setStyle(this.el.nativeElement, 'color', '#000');
-    this.backgroundColor = "transparent";
+    this.backgroundColor = this.defaultBackground;
     this.color = "#000";
     this.transition = "all .5s ease-and-out";
   }
