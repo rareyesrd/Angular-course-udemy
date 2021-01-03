4f54332482db8d549dfeea2e0fff1ace6406c3c7
Rafael Reyes <rafaelalexanderr5@gmail.com>
2021-01-03 12:10:28 -0400

    Using HostBinding on directives

--- directives-start/src/app/directive/better-highlight.directive.ts
+++ directives-start/src/app/directive/better-highlight.directive.ts
@@ -1,26 +1,40 @@
-import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  OnInit,
+  Renderer2,
+} from "@angular/core";
 
 @Directive({
-  selector: '[appBetterHighlight]'
+  selector: "[appBetterHighlight]",
 })
-export class BetterHighlightDirective implements OnInit{
+export class BetterHighlightDirective implements OnInit {
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
 
-  constructor(private el: ElementRef,private renderer: Renderer2) { }
-
-  ngOnInit():void {
+  ngOnInit(): void {
     // this.renderer.setStyle(this.el.nativeElement, 'background-color', 'blue');
     // this.renderer.setStyle(this.el.nativeElement, 'color', '#fff');
-    this.renderer.setStyle(this.el.nativeElement, 'padding', '20px');
+    this.renderer.setStyle(this.el.nativeElement, "padding", "20px");
   }
-  
-  @HostListener('mouseenter') mouseover() {
-    this.renderer.setStyle(this.el.nativeElement, 'background-color', 'blue');
-    this.renderer.setStyle(this.el.nativeElement, 'color', '#fff');
+  @HostBinding("style.backgroundColor") backgroundColor: string = "transparent";
+  @HostBinding("style.color") color: string = "#000";
+  @HostBinding("style.transition") transition: string = "all 1s ease";
+
+  @HostListener("mouseenter") mouseover() {
+    // this.renderer.setStyle(this.el.nativeElement, 'background-color', 'blue');
+    // this.renderer.setStyle(this.el.nativeElement, 'color', '#fff');
+    this.backgroundColor = "blue";
+    this.color = "#fff";
+    this.transition = "all .5s ease";
   }
 
-  @HostListener('mouseleave') mouseleave() {
-    this.renderer.setStyle(this.el.nativeElement, 'background-color', 'transparent');
-    this.renderer.setStyle(this.el.nativeElement, 'color', '#000');
-  
+  @HostListener("mouseleave") mouseleave() {
+    // this.renderer.setStyle(this.el.nativeElement, 'background-color', 'transparent');
+    // this.renderer.setStyle(this.el.nativeElement, 'color', '#000');
+    this.backgroundColor = "transparent";
+    this.color = "#000";
+    this.transition = "all .5s ease-and-out";
   }
 }
