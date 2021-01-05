bb9cb69930b69159611e2cdc57d32befe936789c
Rafael Reyes <r.reyes@caribemedia.com.do>
2021-01-05 16:18:48 -0400

    Communicating the data with the recipeService

--- project-course/src/app/Components/recipes/recipe-list/recipe-list.component.ts
+++ project-course/src/app/Components/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,18 +8,12 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('Test Name', 'Test Description', 'https://www.telegraph.co.uk/content/dam/food-and-drink/2019/01/11/TELEMMGLPICT000185036503_trans_NvBQzQNjv4Bq8m3xuhMyFOjUOkuEnTdW-M-bhHwB87o-r13mliye62g.jpeg?imwidth=1400'),
-    new Recipe('Another Test case', 'Test Description', 'https://sevilla.abc.es/gurme/wp-content/uploads/sites/24/2012/01/comida-rapida-casera.jpg')
-  ];
-  constructor() { }
+  recipes: Recipe[];
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
-  }
+    this.recipes = this.recipeService.getRecipes();
 
-  onRecipeSelected(recipe: Recipe): void{
-    this.recipeWasSelected.emit(recipe);
   }
 
 }
--- project-course/src/app/Components/recipes/recipe.service.ts
+++ project-course/src/app/Components/recipes/recipe.service.ts
@@ -1,10 +1,20 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+
+import { Recipe } from './recipe.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
-
+  recipeSelected = new EventEmitter<Recipe>();
   
+  recipes: Recipe[] = [
+    new Recipe('Test Name', 'Test Description', 'https://www.telegraph.co.uk/content/dam/food-and-drink/2019/01/11/TELEMMGLPICT000185036503_trans_NvBQzQNjv4Bq8m3xuhMyFOjUOkuEnTdW-M-bhHwB87o-r13mliye62g.jpeg?imwidth=1400'),
+    new Recipe('Another Test case', 'Test Description', 'https://sevilla.abc.es/gurme/wp-content/uploads/sites/24/2012/01/comida-rapida-casera.jpg')
+  ];
+
+  getRecipes(): Recipe[] {
+    return this.recipes.slice();
+  }
   
 }
