f38a2135f23e232e97a01bebb1cb04ac752a0409
Rafael Reyes <r.reyes@caribemedia.com.do>
2021-01-15 16:13:03 -0400

    Getting the formData

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -15,15 +15,36 @@ export class AppComponent {
     'Male',
     'Female'
   ];
-  suggestUserName() {
+  showDataForm = false;
+
+  user = {
+      name: '',
+      mail: '',
+      answer: '',
+      gender: '',
+      secret: ''
+    };
+  suggestUserName(form: any): void {
     const suggestedName = 'Superuser';
+    form.patchValue({
+      gender: this.genders[0],
+      userData: {
+        username: suggestedName,
+      },
+    });
   }
 
   // onSubmit(form: NgForm): void {
-  //   console.log(this.s);
+  //   console.log(this.form);
   // }
 
   onSubmit(): void {
+    this.showDataForm = true;
     console.log(this.signUp);
+    this.user.name = this.signUp.value.userData.username;
+    this.user.mail = this.signUp.value.userData.email;
+    this.user.answer = this.signUp.value.defaultQuestion;
+    this.user.secret = this.signUp.value.secret;
+    this.user.gender = this.signUp.value.gender;
   }
 }
