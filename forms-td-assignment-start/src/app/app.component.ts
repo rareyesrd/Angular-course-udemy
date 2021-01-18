2d3a4461af48c97bd7ff369f4f656b3278e21600
Rafael Reyes <r.reyes@caribemedia.com.do>
2021-01-18 16:35:39 -0400

    working on the assignment

--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  @ViewChild('f') handleForm: NgForm;
+
+  data = {
+    email: '',
+    level: '',
+    password: '',
+  };
+
+
+  onSubmit(): void {
+    console.log(this.handleForm);
+    this.data.email = this.handleForm.value.email;
+    this.data.level = this.handleForm.value.level;
+    this.data.password = this.handleForm.value.password;
+
+
+    this.handleForm.resetForm();
+  }
 }
