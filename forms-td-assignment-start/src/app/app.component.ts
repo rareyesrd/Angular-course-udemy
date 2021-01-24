dbb0eb84293ef01acdcc744237e45dd3fe09006a
Rafael Reyes <rafaelalexanderr5@gmail.com>
2021-01-24 17:25:34 -0400

    [ADD] form reactive folder

--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -1,28 +1,22 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
-
-  @ViewChild('f') handleForm: NgForm;
+  @ViewChild("f") handleForm: NgForm;
 
   showData = false;
-  subscriptions = [
-    'Basic',
-    'Advanced',
-    'Pro'
-  ];
+  subscriptions = ["Basic", "Advanced", "Pro"];
 
   data = {
-    email: '',
-    level: '',
-    password: '',
+    email: "",
+    level: "",
+    password: "",
   };
-
 
   ngOnInit(): void {
     // console.log(this.handleForm)
@@ -33,13 +27,14 @@ export class AppComponent implements OnInit {
     this.data.email = this.handleForm.value.email;
 
     // Check if select options was selected and setting a default value to Advanced
-    !this.handleForm.controls.level.touched || this.handleForm.controls.level.value === '' ? this.data.level = 'Advanced' : this.data.level = this.handleForm.value.level;
-
+    !this.handleForm.controls.level.touched ||
+    this.handleForm.controls.level.value === ""
+      ? (this.data.level = "Advanced")
+      : (this.data.level = this.handleForm.value.level);
 
     this.data.password = this.handleForm.value.password;
     this.showData = !this.showData;
     console.log(this.data);
     // this.handleForm.resetForm();
-
   }
 }
