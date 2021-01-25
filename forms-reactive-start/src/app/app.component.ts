707e9fd38ee4842a97015f559a7955aa71f0159c
Rafael Reyes <rafaelalexanderr5@gmail.com>
2021-01-25 03:28:16 -0400

    Reactive form

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormArray, FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  genders = ['male', 'female'];
+export class AppComponent implements OnInit {
+  genders = ["Male", "Female"];
+  signUpForm: FormGroup;
+
+  ngOnInit(): void {
+    this.signUpForm = new FormGroup({
+      'userdata': new FormGroup({
+        'username': new FormControl(null, Validators.required),
+        'email': new FormControl(null, [Validators.required, Validators.email]),
+      }),
+      'gender': new FormControl("Male"),
+      'hobbies': new FormArray([])
+    });
+  }
+
+  onAddHubby(){
+    let control = new FormControl(null, Validators.required);
+    (<FormArray>this.signUpForm.get('hobbies')).push(control);
+  }
+  getControls(){
+    return (<FormArray>this.signUpForm.get('hobbies')).controls
+  }
+
+  onSubmit() {
+    console.log(this.signUpForm);
+  }
 }
