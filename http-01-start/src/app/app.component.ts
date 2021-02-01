38b13df8db00b59f97ef613ea59ef3fddbeca3ec
Rafael Reyes <r.reyes@caribemedia.com.do>
2021-02-01 16:57:09 -0400

    [POST] information to realtime database - firebase

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -9,13 +9,15 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent implements OnInit {
   loadedPosts = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
     console.log(postData);
+    this.http.post('https://ng-complete-guide-5dc83-default-rtdb.firebaseio.com/posts.json', postData)
+    .subscribe(responseData => console.log(responseData));
   }
 
   onFetchPosts() {
