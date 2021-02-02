20b4360829d5cedb826cb1099796e6a5c09d9086
Rafael Reyes <r.reyes@caribemedia.com.do>
2021-02-02 09:30:40 -0400

    Handling errors with messages

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -1,20 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Post } from './posts.model';
 import { PostService } from './post.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   loadedPosts: Post[] = [];
   isFetching = false;
   error = null;
+
+  private errorSub: Subscription;
   constructor(private postService: PostService) { }
 
   ngOnInit() {
-    this.postService.error.subscribe(errorMessage => this.error = errorMessage);
+    this.errorSub = this.postService.error.subscribe(errorMessage => this.error = errorMessage);
     this.fetchPosts();
   }
 
@@ -37,5 +40,11 @@ export class AppComponent implements OnInit {
       }, error => {
         this.error = `${error.error.error} status ${error.status}`;
       });
+  }
+  onErrorHandle() {
+    this.error = null;
+  }
+  ngOnDestroy(): void {
+    this.errorSub.unsubscribe();
   }
 }
--- http-01-start/src/app/post.service.ts
+++ http-01-start/src/app/post.service.ts
@@ -1,12 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
-import { map } from 'rxjs/Operators';
+import { Subject, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/Operators';
 import { Post } from './posts.model';
 
 @Injectable({ providedIn: 'root' })
 export class PostService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
   error = new Subject<string>();
   onCreateandStorePost(postData: { title: string; content: string }) {
     // Send Http request
@@ -15,7 +15,7 @@ export class PostService {
         'https://ng-complete-guide-5dc83-default-rtdb.firebaseio.com/posts.json',
         postData
       )
-      .subscribe((responseData) => responseData, error =>  this.error = error.message);
+      .subscribe((responseData) => responseData, error => this.error = error.message);
   }
 
   onFecthPost() {
@@ -32,11 +32,14 @@ export class PostService {
             }
           }
           return postsArray;
+        }),
+        catchError(errorRes => {
+          return throwError(errorRes);
         })
       );
   }
 
   deletePost() {
-      return this.http.delete('https://ng-complete-guide-5dc83-default-rtdb.firebaseio.com/posts.json');
+    return this.http.delete('https://ng-complete-guide-5dc83-default-rtdb.firebaseio.com/posts.json');
   }
 }
